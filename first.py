862895c1e2f713afa01ed8e6d173bc05c9656e96
Tejas-TY <tytejas4@gmail.com>
2026-07-13 23:20:24 +0530

    learnt about operations

--- first.py
+++ first.py
@@ -1,4 +1,22 @@
+#print(not False)
+#print(not True)
+
 a=10
-b=1
-sum=a+b
-print(sum)
+b=1000
+
+#print(not (a>b))
+
+a=False
+b=False
+#
+# 
+# 
+#print(a and b)
+
+
+#print(a or b)
+
+c=int("20")
+d=2.77
+
+#sum= print(c+d)
