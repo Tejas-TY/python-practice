83e3cd0dbb336ee980b634c08d81cdca8ffe2efd
Tejas-TY <tytejas4@gmail.com>
2026-07-14 23:18:05 +0530

    problem solving

--- first.py
+++ first.py
@@ -1,30 +1,6 @@
-#print(not False)
-#print(not True)
-
-a=10
-b=1000
-
-#print(not (a>b))
-
-a=False
-b=False
-#
-# 
-# 
-#print(a and b)
-
-
-#print(a or b)
-
-c=int("20")
-d=2.77
-
-#sum= print(c+d)
-
-
 num1 = int(input("Enter the first number :"))
 num2 = int(input("Enter the second number :"))
 
-sum = num1 + num2
+sub = num1 - num2
 
-print(sum)
+print(sub)
