ab15dc02cbe8c11b118adbbafe34fb3bda08ec32
Tejas-TY <tytejas4@gmail.com>
2026-07-19 23:17:48 +0530

    problem

--- first.py
+++ first.py
@@ -1,6 +1,6 @@
 num1 = int(input("Enter the first number :"))
 num2 = int(input("Enter the second number :"))
 
-sub = num1 - num2
+sub = num1 * num2
 
 print(sub)
--- second.py
+++ second.py
@@ -1,2 +1,5 @@
 name = input("enter your name")
 print("length of your name is", len(name))
+
+age = "14" 
+print(len(age))
