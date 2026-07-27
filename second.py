e64ab9b11614b7eaa844aa144ebff0c6ec1e7b54
Tejas-TY <tytejas4@gmail.com>
2026-07-27 23:42:34 +0530

    pratice

--- second.py
+++ second.py
@@ -1,2 +1,9 @@
 num = int(input("Enter the number : "))
-print("even" if num % 2 == 0 else "odd")
+#print("even" if num % 2 == 0 else "odd")
+
+if (num == 7):
+    print("Tala")
+elif(num==18):
+    print("King")
+else:
+    print(num)
